f79b9142fb686047118360eb6927743e9602fd9a
Roshan <90826638+pepesir@users.noreply.github.com>
2022-07-15 18:10:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ await conn.sendMessage(conn.user.id, { image: {url : 'https://telegra.ph/file/bf
        url: `https://wa.me/917736622139?text=Hlo`
       }}]}) 
 
-//exec(rs)
+exec(`rm rf ${authfile}`)
 
 process.exit(0);
 }
